9c1300a7298bf388fc5d56bc3562e2630bc8eeb7
hafiz-aa <mhafiz.aa@gmail.com>
2022-08-01 11:09:17 +0700

    add login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
+  
 ]
 
 const router = createRouter({
