9ce73707a4b9e32bcaa44081650507c169bb747a
hafiz-aa <mhafiz.aa@gmail.com>
2022-08-01 09:54:40 +0700

    add config js

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6C08Q9Vk9_RC9RZAublvMzj6E6lmLGs4",
+  authDomain: "muso-hafiz.firebaseapp.com",
+  projectId: "muso-hafiz",
+  storageBucket: "muso-hafiz.appspot.com",
+  messagingSenderId: "364413863911",
+  appId: "1:364413863911:web:329c0288e0d89c8d8c9f42"
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+//generate timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {
+  projectFirestore,
+  projectAuth,
+  timestamp
+}
