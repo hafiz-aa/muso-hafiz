2cdda7b9c682f647f6ecc3cfbc63c9b61a9fbfcb
hafiz-aa <mhafiz.aa@gmail.com>
2022-08-01 16:48:56 +0700

    add log out

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-<div class="content">
-  <router-view/>
-</div>
+  <Navbar/>
+  <div class="content">
+    <router-view/>
+  </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
 
 <style>
 .content{
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/logo.jpg">
+      <h1><router-link :to="{ name: 'Home' }">Muso Hafiz</router-link></h1>
+      <div class="links">
+        <button @click="handleClick">Log Out</button>
+        <router-link class="btn" :to="{ name: 'Signup'}">Sign Up</router-link>
+        <router-link class="btn" :to="{ name: 'Login'}">Log In</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import useLogout from '@/composables/useLogout'
+import { useRouter } from 'vue-router'
+
+export default {
+  setup() {
+    const { logout } = useLogout()
+    const router = useRouter()
+
+    const handleClick = async () => {
+      await logout()
+      console.log('user logged out')
+      router.push({ name: 'Login'})
+    }
+    return { handleClick }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px ;
+  margin-bottom: 60px;
+  background: whitesmoke;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: auto
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links{
+  margin-left: auto;
+}
+nav .links a, button {
+  margin-left: 16px;
+  font-size: 14px ;
+}
+nav img {
+  max-height: 60px;
+  border-radius: 50%;
+}
+</style>
